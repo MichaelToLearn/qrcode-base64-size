4c2699175642149816bd943addaf0245bdc24c54
shengdi.sd <shengdi.sd@antgroup.com>
2023-04-23 11:10:05 +0800

    chore: update version to 1.0.2

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,14 @@ export interface IOptions {
   typeNumber?: number;
   errorCorrectLevel?: 'H' | 'Q' | 'M' | 'L';
   size?: number;
+  /**
+   * 二维码背景色，格式 #ffffff
+   */
   backgroundColor?: string;
+  /**
+   * 二维码前景色，格式 #ffffff
+   * 指方块的颜色
+   */
   foregroundColor?: string;
 }
 export function drawImg(text: string, options: IOptions): string;
